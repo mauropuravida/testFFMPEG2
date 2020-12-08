17c65e7ab9ca40c0b9161806c758fe0b7f11f612
mauro <mauropuravida@gmail.com>
2020-12-08 19:26:21 -0300

    Update project

--- app/src/main/java/com/example/testffmpeg2/MainActivity.java
+++ app/src/main/java/com/example/testffmpeg2/MainActivity.java
@@ -1,98 +1,106 @@
 package com.example.testffmpeg2;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.hardware.Camera;
+import android.content.Context;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
+import android.text.format.DateUtils;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.arthenica.mobileffmpeg.Config;
-import com.arthenica.mobileffmpeg.FFmpeg;
-
-import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_CANCEL;
-import static com.arthenica.mobileffmpeg.Config.RETURN_CODE_SUCCESS;
+import android.view.WindowManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import java.io.File;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DESKTOP_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.185 Mobile Safari/537.36";
+    private static String TAG= "sad";
+
+    private int getScale(){
+        Display display = ((WindowManager) getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        int width = display.getWidth();
+        Double val = new Double(width)/new Double(360);
+        val = val * 100d;
+        return val.intValue();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        new WebView(getApplicationContext()).clearCache(true);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        Log.i("WebViewActivity", "UA: " + myWebView.getSettings().getUserAgentString());
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        // Configure the client to use when opening URLs
+        myWebView.setWebViewClient(new WebViewClient());
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setDomStorageEnabled(true);// Add this
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onPermissionRequest(PermissionRequest request) {
+                request.grant(request.getResources());
             }
         });
-
-        //Dir internal video file input
-        String filein = getExternalFilesDir(null).getAbsoluteFile()+"/gui.mp4";
-        //Dir video file out
-        String fileout = getExternalFilesDir(null).getAbsoluteFile()+"/gui2.mp4";
-
-        //Copy filein to fileout
-        //int rc = FFmpeg.execute("-i "+filein+" -c:v mpeg4 "+ fileout);
-
-        //Local streaming filein, you need execute "ffplay udp://192.168.0.194:23000" command in pc
-        //int rc = FFmpeg.execute("-i "+filein+" -vcodec mpeg4 -f mpegts udp://192.168.0.194:23000");
-
-        if(ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
-
-            Camera.open();
-        }else{
-            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA}, 0);
-        }
-
-        //Stream 35 seconds using android camera
-        int rc = FFmpeg.execute("-f android_camera -i 0:0 -r 30 -pixel_format bgr0 -t 00:00:35 -vcodec mpeg4 -f mpegts udp://192.168.0.194:23000");
-        if (rc == RETURN_CODE_SUCCESS) {
-            Log.i(Config.TAG, "Command execution completed successfully.");
-        } else if (rc == RETURN_CODE_CANCEL) {
-            Log.i(Config.TAG, "Command execution cancelled by user.");
-        } else {
-            Log.i(Config.TAG, String.format("Command execution failed with rc=%d and the output below.", rc));
-            Config.printLastCommandOutput(Log.INFO);
-        }
+        myWebView.getSettings().setUserAgentString(DESKTOP_USER_AGENT);
+        myWebView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        myWebView.clearHistory();
+        myWebView.clearFormData();
+        //myWebView.clearSslPreferences();
+        myWebView.clearCache(true);
+        myWebView.getSettings().setAppCacheEnabled(false);
+        deleteDatabase("webview.db");
+        deleteDatabase("webviewCache.db");
+        myWebView.setPadding(0, 0, 0, 0);
+        myWebView.setInitialScale(getScale());
+        myWebView.loadUrl("http://159.65.97.50/example/js/rtp-forwarder/");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    static int clearCacheFolder(final File dir, final int numDays) {
+
+        int deletedFiles = 0;
+        if (dir!= null && dir.isDirectory()) {
+            try {
+                for (File child:dir.listFiles()) {
+
+                    //first delete subdirectories recursively
+                    if (child.isDirectory()) {
+                        deletedFiles += clearCacheFolder(child, numDays);
+                    }
+
+                    //then delete the files and subdirectories in this dir
+                    //only empty directories can be deleted, so subdirs have been done first
+                    if (child.lastModified() < new Date().getTime() - numDays * DateUtils.DAY_IN_MILLIS) {
+                        if (child.delete()) {
+                            deletedFiles++;
+                        }
+                    }
+                }
+            }
+            catch(Exception e) {
+                Log.e(TAG, String.format("Failed to clean the cache, error %s", e.getMessage()));
+            }
+        }
+        return deletedFiles;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /*
+     * Delete the files older than numDays days from the application cache
+     * 0 means all files.
+     */
+    public static void clearCache(final Context context, final int numDays) {
+        Log.i(TAG, String.format("Starting cache prune, deleting files older than %d days", numDays));
+        int numDeletedFiles = clearCacheFolder(context.getCacheDir(), numDays);
+        Log.i(TAG, String.format("Cache pruning completed, %d files deleted", numDeletedFiles));
     }
 }
